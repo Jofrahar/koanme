13e9c0001e4414b61fe1b446bc8446ef3f93e916
John Hare <mostly_me@hotmail.com>
2020-11-05 18:56:10 +0000

    Initial commit

---
+++ src/main/java/uk/co/jfh/koanme/app/KoanmeApplication.java
@@ -0,0 +1,8 @@
+package uk.co.jfh.koanme.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoanmeApplication {
+
+}
---
+++ src/test/java/uk/co/jfh/koanme/app/integration/tests/SpringApplicationTest.java
@@ -0,0 +1,20 @@
+package uk.co.jfh.koanme.app.integration.tests;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+public class SpringApplicationTest {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    void checkApplicationContextLoaded() {
+	assertTrue(context != null);
+    }
+}
